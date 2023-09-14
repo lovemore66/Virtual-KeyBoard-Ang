c4a12b8a666b92b8ef59622bdd5fb59b1af008d1
Lovemore Sinkonde <lovemore.sinkonde@clickatell.com>
2023-09-14 21:45:22 +0200

    caps method

--- src/components/virtual-keyboard/virtual-keyboard.component.ts
+++ src/components/virtual-keyboard/virtual-keyboard.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VirtualKeyboardComponent implements OnInit {
   inputText: string = '';
+  isShiftOn: boolean = false;
+  isCapsLocksOn: boolean = false;
+  isControlOn: boolean = false;
+
   keyboardLayout: any[][] = [
     [
       { key: '1', shift: '!' },
@@ -20,7 +24,7 @@ export class VirtualKeyboardComponent implements OnInit {
       { key: '9', shift: '(' },
       { key: '0', shift: ')' },
       { key: '-', shift: '_' },
-      { key: '+', shift: '+' },
+      { key: '=', shift: '+' },
     ],
     [
       { key: 'Tab' },
@@ -46,34 +50,47 @@ export class VirtualKeyboardComponent implements OnInit {
       { key: 'h' },
       { key: 'k' },
       { key: 'l' },
-      { key: 'Enter' }],
+      { key: 'Enter' },
+    ],
     [
       { key: 'Shift' },
       { key: 'z' },
       { key: 'x' },
       { key: 'c' },
       { key: 'v' },
-      { key: 'space' },
+      { key: 'Space' },
       { key: 'b' },
       { key: 'n' },
       { key: 'm' },
-      { key: 'Ctrl' }],
+      { key: 'Ctrl' },
+    ],
   ];
   constructor() {}
 
   ngOnInit(): void {}
 
-  onKeyClick(key: string): void {
+  changeToUppercase(key: string) {
+    if (this.isCapsLocksOn) {
+      return key.toUpperCase();
+    }
+    return key;
+  }
+
+  onKeyClick(key: string, shift: string): void {
     if (key === 'Backspace') {
       this.inputText = this.inputText.slice(0, -1);
     } else if (key === 'Shift') {
-      // Handle shift key logic (you can implement shift functionality here)
-    } else if (key === ' ') {
-      // Handle space key by adding a space to the input text
+      this.isShiftOn = !this.isShiftOn;
+    } else if (key === 'Caps Lock') {
+      this.isCapsLocksOn = !this.isCapsLocksOn;
+    } else if (key === 'Space') {
       this.inputText += ' ';
     } else {
-      // Handle regular key press by appending the key to the input text
-      this.inputText += key;
+      if (this.isShiftOn && shift !== undefined) {
+        this.inputText += this.changeToUppercase(shift);
+      } else {
+        this.inputText += this.changeToUppercase(key);
+      }
     }
   }
 }
